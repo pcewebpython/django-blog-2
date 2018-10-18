64f617d209937e4c5ab217cf350705fede7c18c0
Joseph Schilz <joseph@schilz.org>
2018-10-18 09:26:15 -0700

    Follow lesson 6.

---
+++ myblog/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from myblog.models import Post
+
+
+class PostTestCase(TestCase):
+    fixtures = ['myblog_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
