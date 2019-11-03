718940cfd6b6d25cbdc7dbd310e3ccc106ddccba
JeroenSweerts <jeroensweerts@hotmail.com>
2019-11-03 18:04:11 +0100

    Version ready for deployment

--- blogging/urls.py
+++ blogging/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('', stub_view, name="blog_index"),
     path('',list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
-    path('',post_new,name='post-add')
+    path('',post_new,name='post-add'),    	
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
