d97a33924de51b4be5988cb177d11c7c67f6aae5
mmanc125uw <mmanc125@uw.edu>
2020-09-29 09:55:48 -0400

    sp_py230 lesson7 activity, begin list view

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,10 @@
-# polling/urls.py
-
 from django.urls import path
-from blogging.views import list_view, detail_view
+
+from blogging.views import stub_view
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
+    path('', stub_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
+
+
--- blogging/views.py
+++ blogging/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
-from django.http import Http404
-from polling.models import Poll
+from blogging.models import Post, Category
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
-def list_view(request):
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polling/list.html', context)
 
-def detail_view(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
-            poll.score += 1
-        else:
-            poll.score -= 1
-        poll.save()
-
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+def stub_view(request, *args, **kwargs):
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
