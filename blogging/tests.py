cefe3e287a5e5825a9a17f20fd180f621fda0021
mmanc125uw <mmanc125@uw.edu>
2020-09-29 14:28:01 -0400

    sp_py230 test rerun

--- blogging/tests.py
+++ blogging/tests.py
@@ -7,6 +7,19 @@ from django.utils.timezone import utc
 from blogging.models import Post
 from blogging.models import Category
 
+
+class PostTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
 class CategoryTestCase(TestCase):
 
     def test_string_representation(self):
