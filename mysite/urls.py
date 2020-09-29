e81934c58f0545ae4e43d619bafabbb2da529510
mmanc125uw <mmanc125@uw.edu>
2020-09-29 10:03:22 -0400

    sp_py230 lesson7 activity, updated urls.py for home

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include  # <-- Make sure you have both of these im
 
 
 urlpatterns = [
+    path('', include('blogging.urls')),  # <-- add this
     path('polling/', include('polling.urls')),  # <-- Add this
     path('blogging/', include('blogging.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
