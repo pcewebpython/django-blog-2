61e03a5088569f61bff0347d3dfea7bf2febb0f0
stellie <kims82@gmail.com>
2020-12-21 14:52:53 -0800

    initial status post lesson 6

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from blogging.models import Post, Category
 
+from blogging.models import Post
 
 admin.site.register(Post)
-admin.site.register(Category)
--- polling/views.py
+++ polling/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
 
+
 def list_view(request):
     context = {'polls': Poll.objects.all()}
     return render(request, 'polling/list.html', context)
+
 
 def detail_view(request, poll_id):
     try:
@@ -12,8 +14,8 @@ def detail_view(request, poll_id):
     except Poll.DoesNotExist:
         raise Http404
 
-    if request.method == "POST":
-        if request.POST.get("vote") == "Yes":
+    if request.method == 'POST':
+        if request.POST.get('vote') == 'Yes':
             poll.score += 1
         else:
             poll.score -= 1
