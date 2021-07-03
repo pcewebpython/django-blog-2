22c6f598cba9b9455b8b0778d9f1e219def454c4
Ian Mayther <maytheri@gmail.com>
2021-07-02 21:13:22 -0700

    Brought over existing blog files

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
-
 urlpatterns = [
-    path('', include('blogging.urls')),
+    path('', include('blogging.urls')),    
     path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
