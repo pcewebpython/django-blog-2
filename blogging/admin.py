20f182478d0b0490ac2f013dcfb92084fbda3965
Ian Mayther <maytheri@gmail.com>
2021-07-02 21:58:12 -0700

    Completed adminModels ?

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,3 +4,13 @@ from blogging.models import Post, Category
 
 admin.site.register(Post)
 admin.site.register(Category)
+
+class PostAdmin(admin.ModelAdmin):
+    fields = (
+        'title', 'text', 'author', 'created_date', 'modified_date', 'published_date'
+    )
+
+class Category(admin.ModelAdmin):
+    fields = (
+        'name', 'description', 'posts'
+    )
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pkm=h&_#xt%)@ohx+v8xxd5mw5-2s897p2aea3rj-&nftng4s7'
+SECRET_KEY = 'xak=ca*e6hvh8q5hgfz5l9ees)_pxjif0)ui!ikifg4!enjk+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
